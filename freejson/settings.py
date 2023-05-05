579e8303fc7781d232def4a91f79b275122cf798
Pankaj <pankajkushwaha251@gmail.com>
2023-05-05 20:08:03 +0530

    final commit

--- freejson/settings.py
+++ freejson/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-!-=qcf5&%ci4hak^$kn7=2mt)5o*ru@6cp-a)b&dixq+b-734a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
